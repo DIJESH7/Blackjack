733ee8aa3fe2ca96bed115ff9a0752e37e224979
erace-code <57083860+erace-code@users.noreply.github.com>
2019-10-29 19:55:56 -0500

    Edited Rank names

--- include/enums.h
+++ include/enums.h
@@ -1,20 +1,10 @@
-///*
-enum Rank //face
-{
-   Ace = 1, Deuce = 2, Trey = 3, Four = 4, Five = 5, Six = 6, Seven = 7,
-   Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13
-};
-//*/
-
-/*
-enum Rank 
+enum Rank
 {
    Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7,
-   Eight = 8, Nine = 9, Ten = 10, Jack = 10, Queen = 10, King = 10
+   Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13
 };
-*/
 
-enum Suit 
+enum Suit
 {
    Spades, Hearts, Clubs, Diamonds
 };
