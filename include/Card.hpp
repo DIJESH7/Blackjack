140972eb58ccb26bc8bb5f30af1fda7c888dad45
erace-code <57083860+erace-code@users.noreply.github.com>
2019-11-02 11:49:04 -0500

    Add files via upload

---
+++ include/Card.hpp
@@ -0,0 +1,73 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <iostream>
+//#include <string>
+#include "enums.h"
+
+using namespace std;
+
+class Card
+{
+public:
+
+  Card()
+  {
+
+  }
+
+  Card(Rank rank, Suit suit)
+  {
+      rank_ = rank;
+      suit_ = suit;
+  }
+
+  // prints the card info in a string
+  void display() 
+  {
+      const char * rankText[] = { " ", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+      const char * suitText[] = { "Spades", "Hearts", "Clubs", "Diamonds" };
+      
+      cout << rankText[rank_] << " of " << suitText[suit_] << endl;
+  }
+
+  void setValue(int val)
+  {
+     value = val;
+  }
+
+  void setRank(Rank rank)
+  {
+     rank_ = rank;
+  }
+
+  void setSuit(Suit suit)
+  {
+     suit_ = suit;
+  }
+
+  int getValue()
+  {
+     return value;
+  }
+
+  Rank getRank()
+  {
+     return rank_;
+  }
+
+  Suit getSuit()
+  {
+     return suit_;
+  }
+
+private:
+  Rank rank_;
+  Suit suit_;
+  int value;
+  
+};
+
+#endif
+
