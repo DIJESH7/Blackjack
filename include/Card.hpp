5c2e0dcc90bce51148ef626e9c258d4d4d055762
erace-code <57083860+erace-code@users.noreply.github.com>
2019-12-04 20:30:39 -0600

    Commented card object members

--- include/Card.hpp
+++ include/Card.hpp
@@ -21,11 +21,17 @@ public:
       isFaceUp = false;
   }
 
+   /**
+   * Displays the card's rank and suit
+   */
   void display() 
   {
      std::cout << rank_ << " of " << suit_ << std::endl;
   }
 
+   /**
+   * Returns a card's info in a string format
+   */
   std::string printCard()
   {
     std::string result;
@@ -36,28 +42,49 @@ public:
     return result;
   }
 
+   /**
+   * Changes the boolean isFaceUp from false to true
+   */
   void flip()
   {
     isFaceUp =!isFaceUp;
   }
+  
+   /**
+   * Sets the value of the card
+   */
   void setValue(int val)
   {
      value = val;
   }
   
+   /**
+   * Returns the value of the card
+   */
   int getValue()
   {
     return value;
   }
+
+  /**
+   * Returns the rank of the card.
+   */
   char getRank()
   {
     return rank_;
   }
+  
+   /**
+   * Returns the suit of the card.
+   */
   char getSuit()
   {
     return suit_;
   }
-
+  
+  /**
+   * Sets the value, rank, and suit of card.
+   */
   void setInfo(int v, char r, char s)
   {
     value = v;
